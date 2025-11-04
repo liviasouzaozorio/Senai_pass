a40c5df9c0713f2aa6f686f3732840f4e6edbb0e
Livia Vitoria <liviasouzaozorio@gmail.com>
2025-11-04 12:22:33 -0300

    Alterações no UX

--- app/src/main/java/com/example/senaipass/LoginActivity.java
+++ app/src/main/java/com/example/senaipass/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.senaipass;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,33 +8,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
-        Button loginButton = findViewById(R.id.login_button);
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Criar a Intent para abrir a HomeActivity
-                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                startActivity(intent); // Inicia a HomeActivity
-                finish(); // Fecha a LoginActivity (opcional)
-            }
-        });
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        //Função de clicar no botão para login, para realizar a autenticação do aluno SENAI
+        Button btnLogin = findViewById(R.id.login_button);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
         });
+
     }
 }
